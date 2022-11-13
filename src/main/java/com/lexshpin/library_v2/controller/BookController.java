cdc4880c5d183d8b05bddd2e554e837eee1b7b27
Alexander Shpin <@lexhpin@gmail.com>
2022-11-13 15:47:50 -0500

    Added logic & page for adding new Book

--- src/main/java/com/lexshpin/library_v2/controller/BookController.java
+++ src/main/java/com/lexshpin/library_v2/controller/BookController.java
@@ -1,9 +1,42 @@
 package com.lexshpin.library_v2.controller;
 
+import com.lexshpin.library_v2.dao.BookDAO;
+import com.lexshpin.library_v2.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
+
+    private BookDAO bookDAO;
+
+    @Autowired
+    public BookController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", bookDAO.index());
+
+        return "books/index";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String addBook(@ModelAttribute("book") Book book) {
+        bookDAO.add(book);
+
+        return "redirect:/books";
+    }
 }
--- src/main/java/com/lexshpin/library_v2/model/Book.java
+++ src/main/java/com/lexshpin/library_v2/model/Book.java
@@ -14,6 +14,8 @@ public class Book {
         this.year = year;
     }
 
+    public Book() {}
+
     public int getId() {
         return id;
     }
