97a687742c207e0f6d0ff7b77666b8cf8dd025d4
Alexander Shpin <@lexhpin@gmail.com>
2022-11-13 15:52:34 -0500

    Added initial logic for displaying chosen book info

--- src/main/java/com/lexshpin/library_v2/controller/BookController.java
+++ src/main/java/com/lexshpin/library_v2/controller/BookController.java
@@ -5,10 +5,7 @@ import com.lexshpin.library_v2.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/books")
@@ -26,6 +23,13 @@ public class BookController {
         model.addAttribute("books", bookDAO.index());
 
         return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookDAO.show(id));
+
+        return "books/show";
     }
 
     @GetMapping("/new")
--- src/main/java/com/lexshpin/library_v2/dao/BookDAO.java
+++ src/main/java/com/lexshpin/library_v2/dao/BookDAO.java
@@ -22,6 +22,10 @@ public class BookDAO {
         return jdbcTemplate.query("SELECT * FROM Book", new BeanPropertyRowMapper<>(Book.class));
     }
 
+    public Book show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class)).stream().findAny().orElse(null);
+    }
+
     public void add(Book book) {
         jdbcTemplate.update("INSERT INTO Book(title, author, year) VALUES(?, ?, ?)", book.getTitle(), book.getAuthor(), book.getYear());
     }
