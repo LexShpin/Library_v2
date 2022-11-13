f9e6c79ead687432a101e07a976f4365278384d5
Alexander Shpin <@lexhpin@gmail.com>
2022-11-13 13:46:34 -0500

    Wrote the model

--- src/main/java/com/lexshpin/library_v2/model/Person.java
+++ src/main/java/com/lexshpin/library_v2/model/Person.java
@@ -1,4 +1,40 @@
 package com.lexshpin.library_v2.model;
 
 public class Person {
+
+    private int id;
+    private String name;
+    private int yearOfBirth;
+
+    public Person(int id, String name, int yearOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Person() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
 }
