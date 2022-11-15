79b0ff0e7dfda4be574989fbe5eab581da041e15
Alexander Shpin <@lexhpin@gmail.com>
2022-11-15 16:33:28 -0500

    Implemented the logic for assigning the books to people

--- src/main/java/com/lexshpin/library_v2/dao/BookDAO.java
+++ src/main/java/com/lexshpin/library_v2/dao/BookDAO.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.Types;
 import java.util.List;
 
 @Component
