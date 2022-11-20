a4f542cc4fbb0c0c47f5b9360ee682917cb842da
lexshpin <lexshpin@gmail.com>
2022-11-19 20:24:50 -0500

    Changing PersonDAO to use Hibernate

--- src/main/java/com/lexshpin/library_v2/dao/PersonDAO.java
+++ src/main/java/com/lexshpin/library_v2/dao/PersonDAO.java
@@ -2,12 +2,12 @@ package com.lexshpin.library_v2.dao;
 
 import com.lexshpin.library_v2.model.Book;
 import com.lexshpin.library_v2.model.Person;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
@@ -20,27 +20,54 @@ public class PersonDAO {
         this.sessionFactory = sessionFactory;
     }
 
+    @Transactional
     public List<Person> index() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("select p from Person p", Person.class).getResultList();
     }
 
+    @Transactional
     public Person show(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.get(Person.class, id);
     }
 
+    @Transactional
     public void add(Person person) {
+        Session session = sessionFactory.getCurrentSession();
 
+        session.persist(person);
     }
 
-    public void update(int id, Person person) {
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        Session session = sessionFactory.getCurrentSession();
 
+        Person person = session.get(Person.class, id);
+
+        person.setName(updatedPerson.getName());
+        person.setYearOfBirth(updatedPerson.getYearOfBirth());
+
+        session.persist(person);
     }
 
+    @Transactional
     public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
 
+        Person person = session.get(Person.class, id);
+
+        session.remove(person);
     }
-    
+
+    @Transactional
     public List<Book> getBooksByPerson(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        List<Book> books = session.createQuery("from Book where assignedTo=id").getResultList();
+
+        return books;
     }
 }
